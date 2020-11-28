0bf7ab03099373af1a22de1067e9596d0d6d32f2
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-11-28 12:35:47 +0000

    Add item types

--- src/inventory/inventory.gd
+++ src/inventory/inventory.gd
@@ -1,14 +1,38 @@
 extends Control
 
+enum ITEM_TYPES {
+	STAFF,
+	BOW,
+	SWORD,
+	SHIELD,
+}
+
 export(int) var slot_count := 9
 export(int) var columns := 3
 
 var blue_staff_texture := preload("res://assets/items/blue_staff.png")
+var green_bow_texture := preload("res://assets/items/green_bow.png")
+var orange_sword_texture := preload("res://assets/items/orange_sword.png")
+var red_shield_texture := preload("res://assets/items/red_shield.png")
 
 func _ready() -> void:
+	randomize()
+	
 	$GridContainer.columns = columns
 	for i in slot_count:
 		var texture_rect = TextureRect.new()
 		texture_rect.name = str(i)
-		texture_rect.texture = blue_staff_texture
+		
+		# pick a random item type
+		var choice: int = ITEM_TYPES.values()[randi() % ITEM_TYPES.size()]
+		match choice:
+			ITEM_TYPES.STAFF:
+				texture_rect.texture = blue_staff_texture
+			ITEM_TYPES.BOW:
+				texture_rect.texture = green_bow_texture
+			ITEM_TYPES.SWORD:
+				texture_rect.texture = orange_sword_texture
+			ITEM_TYPES.SHIELD:
+				texture_rect.texture = red_shield_texture
+		
 		$GridContainer.add_child(texture_rect)

597c23a9d6c57d9cf41bc01064a46bee2208bb3c
Alex Pritchard <alexander.g.pritchard@gmail.com>
2020-11-28 12:31:12 +0000

    Add inventory initialization

--- src/inventory/inventory.gd
+++ src/inventory/inventory.gd
@@ -1,2 +1,14 @@
 extends Control
 
+export(int) var slot_count := 9
+export(int) var columns := 3
+
+var blue_staff_texture := preload("res://assets/items/blue_staff.png")
+
+func _ready() -> void:
+	$GridContainer.columns = columns
+	for i in slot_count:
+		var texture_rect = TextureRect.new()
+		texture_rect.name = str(i)
+		texture_rect.texture = blue_staff_texture
+		$GridContainer.add_child(texture_rect)
